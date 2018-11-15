912d4c5e2ef4ddbf6bc0e7c3bf603369e08b10f0
Jake <jake.trefethen@gmail.com>
2018-11-15 13:28:24 -0800

    initial

---
+++ Mint/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mint
+//
+//  Created by Jake Trefethen on 11/15/18.
+//  Copyright © 2018 Jake Trefethen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
